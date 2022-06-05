bbd8c88aee661b3f6082ab69ddbca28b6852cb2f
pvkhoi901 <quang.khoi.589@gmail.com>
2022-06-05 16:55:32 +0700

    sap xep danh muc

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,5 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
+    public function movie(){
+        return $this->hasMany(Movie::class)->orderBy('id', 'DESC');
+    }
     use HasFactory;
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Scripts -->
     <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -87,6 +87,8 @@
     </div>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+
     <script type="text/javascript">
  
     function ChangeToSlug()
@@ -122,6 +124,28 @@
             document.getElementById('convert_slug').value = slug;
         }
 
+    </script>
+    <script type="text/javascript">
+        $('.order_position').sortable({
+            placeholder : 'ui-state-highlight',
+            update: function(event,ui){
+                var array_id = [];
+                $('.order_position tr').each(function(){
+                    array_id.push($(this).attr('id'));
+                })
+                $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    url:"{{route('resorting')}}",
+                    method:"POST",
+                    data:{array_id:array_id},
+                    success:function(data){
+                        alert('Sắp xếp thứ tự thành công ');
+                    }
+                })
+            }
+        })
     </script>
 </body>
 </html>
