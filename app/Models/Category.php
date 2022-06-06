b47d7a1c381faf188122b2aeec0607afbb9dc31f
pvkhoi901 <quang.khoi.589@gmail.com>
2022-06-06 20:02:00 +0700

    fix movies

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
-    public function movie(){
+    public function movies(){
         return $this->hasMany(Movie::class)->orderBy('id', 'DESC');
     }
     use HasFactory;
